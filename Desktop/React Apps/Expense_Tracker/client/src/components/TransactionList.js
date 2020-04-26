71802beb2f00521704bf59caf7fcc2d6afc76f64
nishant666 <nishants440@gmail.com>
2020-04-27 00:20:29 +0530

    Deploy this app to Github

---
+++ Desktop/React Apps/Expense_Tracker/client/src/components/TransactionList.js
@@ -0,0 +1,21 @@
+import React, { useContext,useEffect } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
+  const { transactions,getTransactions } = useContext(GlobalContext);
+
+  useEffect(()=>{
+    getTransactions();
+  },[]);
+
+  return (
+    <>
+      <h3>Transaction History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
+  )
+}
